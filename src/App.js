f151fc5964d5673ebb42e77fb90c73c58c505376
Festee <fd61273@ubt-uni.net>
2024-05-19 20:39:21 +0200

    Added Carousel(Slider)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Hero from './components/hero/Hero';
 import Destinations from './components/destinations/Destinations'
 import Search from './components/search/Search';
 import Selects from './components/selects/Selects';
+import ImgCarousel from './components/carousel/ImgCarousel';
+
+
 
 
 function App() {
@@ -14,6 +17,7 @@ function App() {
       <Destinations/>
       <Search/>
       <Selects/>
+      <ImgCarousel/>
     </div>
   );
 }
