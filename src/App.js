2c9f5575798c4781fd0b2281dc88191846defcb1
Festee <fd61273@ubt-uni.net>
2024-05-19 20:50:51 +0200

    Added Footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Destinations from './components/destinations/Destinations'
 import Search from './components/search/Search';
 import Selects from './components/selects/Selects';
 import ImgCarousel from './components/carousel/ImgCarousel';
+import Footer from './components/footer/Footer';
 
 
 
@@ -18,6 +19,7 @@ function App() {
       <Search/>
       <Selects/>
       <ImgCarousel/>
+      <Footer/>
     </div>
   );
 }
