68d16a8be07a36c4059c14afe26da8ca3dc9ead4
Festee <fd61273@ubt-uni.net>
2024-05-22 20:18:27 +0200

    Test Search+Json

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,30 +1,59 @@
-import React from 'react'
-import './HeroStyles.css'
-import {AiOutlineSearch} from 'react-icons/ai'
-
-import Video from '../../assets/maldivesVideo.mp4'
+import React, { useState } from 'react';
+import './HeroStyles.css';
+import { AiOutlineSearch } from 'react-icons/ai';
+import Video from '../../assets/maldivesVideo.mp4';
+import citiesData from './cities.json';
 
 function Hero() {
-    return (
-        <div className='hero'>
-            <video autoPlay loop muted id='video'>
-                <source src={Video} type='video/mp4' />
-            </video>
-            <div className="overlay"></div>
-            <div className="content">
-                <h1>First Class Travel</h1>
-                <h2>Top 1% Locations Worldwide</h2>
-                <form className="form">
-                    <div>
-                        <input type="text" placeholder='Search Destinations' />
-                    </div>
-                    <div>
-                        <button><AiOutlineSearch className='icon'/></button>
-                    </div>
-                </form>
-            </div>
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredCities, setFilteredCities] = useState([]);
+
+  const handleSearchChange = (event) => {
+    const searchTerm = event.target.value.toLowerCase();
+    setSearchTerm(searchTerm);
+    const results = citiesData.cities.filter((city) =>
+      city.toLowerCase().startsWith(searchTerm)
+    );
+    setFilteredCities(results);
+  };
+
+  return (
+    <div className="hero">
+      <video autoPlay loop muted id="video">
+        <source src={Video} type="video/mp4" />
+      </video>
+      <div className="overlay"></div>
+      <div className="content">
+        <h1>First Class Travel</h1>
+        <h2>Top 1% Locations Worldwide</h2>
+        <form className="form">
+          <div>
+            <input
+              type="text"
+              placeholder="Search Destinations"
+              value={searchTerm}
+              onChange={handleSearchChange}
+            />
+          </div>
+          <div>
+            <button>
+              <AiOutlineSearch className="icon" />
+            </button>
+          </div>
+        </form>
+         {searchTerm && filteredCities.length > 0 && (
+          <div className="search-results"> 
+          <ul>
+            {filteredCities.map((city) => (
+              <li key={city}>{city}</li>
+            ))}
+          </ul>
         </div>
-    )
+        )}
+      </div>
+     
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
